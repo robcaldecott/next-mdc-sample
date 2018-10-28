2bbd4b2c02f7ee7f4edbfc06ec218b5dc26e6c3a
Rob Caldecott <robert.caldecott@gmail.com>
2018-10-28 21:54:17 +0000

    Next.js example

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@rmwc/button";
+import "../styles/mdc.scss";
+
+export default () => (
+  <div className="mdc-typography">
+    <h1>Next</h1>
+    <Button raised>Button</Button>
+  </div>
+);
